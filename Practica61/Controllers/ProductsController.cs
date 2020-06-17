66c90591a48f07429e9137661e9653bd873dcbb6
FRAN <chiscazo.paco@gmail.com>
2020-06-17 17:02:08 +0200

    Creado proxecto 61.

---
+++ Practica61/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practica61.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // Ruta: /Products/All
+        public IActionResult Index() => Content("ProductsController.Index()");
+
+        // Ruta: /Products/Lenovo-yoga
+        public IActionResult View(string id) => Content($"ProductsController.View('{id}')");
+
+        // Ruta: /Products/Category/Ultrabooks
+        public IActionResult ByCategory(string category) => Content($"ProductsController.ByCategory('{category}')");
+    }
+}
